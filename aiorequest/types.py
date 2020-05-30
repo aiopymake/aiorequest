74a8bb45157c51bdfb34b7ea255a0a092cd4ea40
Volodymyr Yahello <vyahello@gmail.com>
2020-05-30 13:32:13 +0300

    Implement auth types

---
+++ aiorequest/types.py
@@ -0,0 +1,43 @@
+"""The module provides API for credentials."""
+from abc import abstractmethod
+from typing import Any, Dict, Optional, Union
+from punish import AbstractStyle
+
+AnyDict = Dict[Any, Any]
+OptionalStr = Optional[str]
+OptionalAnyDict = Optional[AnyDict]
+AnyUnionDict = Union[AnyDict, Any]
+
+
+class Credentials(AbstractStyle):
+    """The class represents abstraction for credentials."""
+
+    @property
+    @abstractmethod
+    async def username(self) -> str:
+        """Returns a username."""
+        pass
+
+    @property
+    @abstractmethod
+    async def password(self) -> str:
+        """Returns a password."""
+        pass
+
+
+class AuthCredentials(Credentials):
+    """The class represents authorization credentials."""
+
+    def __init__(self, username: str, password: str) -> None:
+        self._username: str = username
+        self._password: str = password
+
+    @property
+    async def username(self) -> str:
+        """Returns auth username."""
+        return self._username
+
+    @property
+    async def password(self) -> str:
+        """Returns auth password."""
+        return self._password

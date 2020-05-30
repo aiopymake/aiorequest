0a216fe75972a02873da3d59bba36553061860a3
Volodymyr Yahello <vyahello@gmail.com>
2020-05-30 13:33:01 +0300

    Implement async HTTP responses

---
+++ aiorequest/responses.py
@@ -0,0 +1,77 @@
+"""The module contains a set of API for HTTP responses types."""
+from typing import Iterable
+import requests
+from punish import AbstractStyle, abstractstyle
+from aiorequest.types import AnyUnionDict
+
+JsonType = AnyUnionDict
+
+
+class ResponseError(Exception):
+    """The class represents HTTP api request response error."""
+
+    pass
+
+
+class Response(AbstractStyle):
+    """The class represents an abstraction of a response from an API request."""
+
+    @abstractstyle
+    async def is_ok(self) -> bool:
+        """Returns `True` if response is `OK` otherwise `False`."""
+        pass
+
+    @abstractstyle
+    async def code(self) -> int:
+        """Returns HTTP response status code."""
+        pass
+
+    @abstractstyle
+    async def as_json(self) -> JsonType:
+        """Returns HTTP response data as dictionary type."""
+        pass
+
+    @abstractstyle
+    def __str__(self) -> str:
+        """Returns HTTP response data as plain data type."""
+        pass
+
+
+class HttpResponse(Response):
+    """The class represents an HTTP response from HTTP API request."""
+
+    def __init__(self, response: requests.Response) -> None:
+        self._response: requests.Response = response
+
+    async def is_ok(self) -> bool:
+        return self._response.ok
+
+    async def code(self) -> int:
+        return self._response.status_code
+
+    async def as_json(self) -> JsonType:
+        return self._response.json()
+
+    def __str__(self) -> str:
+        return self._response.text
+
+
+async def safe_response(
+    response: Response, success_codes: Iterable[int] = (200, 201, 204)
+) -> Response:
+    """Specifies safe response from iterable of success HTTP status codes.
+
+    Args:
+        response: a specific HTTP response
+        success_codes: a list of allowed success codes
+
+    Raises:
+        `ResponseError` if HTTP response contains a set of errors
+    Returns: a response
+    """
+    if await response.code() not in success_codes:
+        raise ResponseError(
+            f"HTTP response contains some errors with '{await response.code()}' status code! "
+            f"Reason: {response}"
+        )
+    return response
